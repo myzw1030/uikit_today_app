01df93cd138927dbee21a97ddcdb6929df157807
Tomokazu Miyazawa <13tmtmtommy@gmail.com>
2024-02-17 14:14:03 +0900

    Initial Commit

---
+++ todayUITests/todayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  todayUITestsLaunchTests.swift
+//  todayUITests
+//
+//  Created by USER on 2024/02/17.
+//
+
+import XCTest
+
+final class todayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
